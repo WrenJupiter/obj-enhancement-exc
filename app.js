82337353d02b24b376d605daac2d6eafbb31b548
wren <keever.payton@gmail.com>
2023-12-11 19:06:16 -0500

    initializing

---
+++ app.js
@@ -0,0 +1,89 @@
+//"Write an ES2015 Version"
+
+//SAME KEYS AND VALUES!
+
+        //function createInstructor(firstName, lastName){
+            //return {
+            //   firstName: firstName,
+            //   lastName: lastName
+            // }
+        // }
+function createInstructor(firstName,lastName){
+    return{
+        firstName,
+        lastName
+        // [firstName] : lastName,
+        // [lastName] : firstName,
+
+    }
+}
+
+
+
+
+//COMPUTED PROPERTY NAMES!
+
+        // var favoriteNumber = 42;
+
+        // var instructor = {
+        //     firstName: "Colt"
+        //   }
+        
+        //   instructor[favoriteNumber] = "That is my favorite!"
+let favNum = 42;
+const instructor = {
+    firstName : "Colt",
+    [favNum] : "That is my favorite!"
+}
+
+
+
+
+//OBJECT METHODS!
+
+        // var instructor = {
+        //     firstName: "Colt",
+        //     sayHi: function(){
+        //       return "Hi!";
+        //     },
+        //     sayBye: function(){
+        //       return this.firstName + " says bye!";
+        //     }
+        //   }
+
+const instruction = {
+    firstName : "Colt",
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return this.firstName + 'says bye!';
+    }
+}
+
+
+
+
+//CREATEANIMAL FUNCTION!
+
+        // Write a function which generates an animal object. The function should accepts 3 arguments:
+        // species: the species of animal (‘cat’, ‘dog’)
+        // verb: a string used to name a function (‘bark’, ‘bleet’)
+        // noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+        // Use one or more of the object enhancements we’ve covered.
+        // const d = createAnimal("dog", "bark", "Woooof!")
+        // // {species: "dog", bark: ƒ}
+        // d.bark()  //"Woooof!"
+
+        // const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+        // // {species: "sheep", bleet: ƒ}
+        // s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species,verb,noise){
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+}
